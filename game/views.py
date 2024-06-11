ab5d75957e118af35b8f5e257682ce9248197a97
BiTech5 <bikikc7@gmail.com>
2024-06-11 15:33:32 +0545

    conecting static file

--- game/views.py
+++ game/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 from . import guess_game
 def home(request):
-    a=guess_game.guess_fun(2)
-    print(a)
-    return HttpResponse(f'<h1>Hello World</h1> {a}')
+    nbr=guess_game.guess_fun(2)
+    print(nbr)
+    return render(request,'main.html',{'nbr':nbr})
